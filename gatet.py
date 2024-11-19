56fa041b7a8058b732493ca493fbf62cb27b69be
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2024-11-19 20:45:02 +0800

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -18,12 +18,12 @@ def Tele(ccx):
 			'origin': 'https://js.stripe.com',
 			'referer': 'https://js.stripe.com/',
 			'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-			'sec-ch-ua-mobile': '?1',
-			'sec-ch-ua-platform': '"Android"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"Windows"',
 			'sec-fetch-dest': 'empty',
 			'sec-fetch-mode': 'cors',
 			'sec-fetch-site': 'same-site',
-			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 	}
 
 	data = f'type=card&billing_details[name]=SHEEESHH+SHEESSH&billing_details[email]=SHEESSH%40gmail.com&card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=0d5df85c-e363-4fb1-8b62-70f7753f65949c292d&muid=b644482a-f109-4de9-95d5-c07b01167f1281c281&sid=11151dee-e062-48e4-9858-b4ebcf0b4d58488d91&pasted_fields=number&payment_user_agent=stripe.js%2F5cb4318496%3B+stripe-js-v3%2F5cb4318496%3B+split-card-element&referrer=https%3A%2F%2Fwww.kaientrails.ca&time_on_page=163573&key=pk_live_51MjvmRFL2ntPTriuBqDXVy7VN1mmXKzOPGIXLbUH9l0a2CFqnndk69W6x9kTxJyhjTVfCmb3Zzm9akEgBZAqedCg00XE4ShyYy'
